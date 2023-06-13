1b79f2c60c35b2b89062034b442fcb21e0effe66
Louis Ruocco <louisruocco1@gmail.com>
2023-06-13 22:17:32 +0100

    wrapped timer in function and added url param

--- script.ps1
+++ script.ps1
@@ -1,5 +1,14 @@
-$minute = 51
-while(($minute = $minute - 1) -ge 0){
-    write-host "Time Remaining: $minute Minutes"
-    start-sleep 60
+function studysession {
+    param (
+        [Parameter(Mandatory)]
+        [string]$url
+    )
+    
+    $minute = 51
+    while(($minute = $minute - 1) -ge 0){
+        write-host "Time Remaining: $minute Minutes"
+        start-sleep 60
+    }
 }
+
+studysession
