081eedc7d32324979ae324b0645fd5a2c24900f1
Louis Ruocco <louisruocco1@gmail.com>
2023-06-15 22:31:47 +0100

    added break timer and output to user

--- script.ps1
+++ script.ps1
@@ -1,16 +1,21 @@
-function studysession {
-    param (
-        [Parameter(Mandatory)]
-        [string]$url
-    )
-    
+function studysession {  
     $minute = 51
     while(($minute = $minute - 1) -ge 0){
         write-host "Time Remaining: $minute Minutes"
-        start-sleep 1
+        start-sleep 60
     }
 
-    start-process msedge $url
+    write-host "Study Session Finished!"
+    write-host "Starting Break timer..."
+    start-sleep 2
+
+    $break = 10
+    while (($break = $break - 1) -ge 0){
+        write-host "Break Time Remaining: $break Minutes"
+        start-sleep 60
+    }
+
+    write-host "Break time has finished! Please type 'studysession' to start another study session"
 }
 
 studysession
